b28da768b18293779d4b0f6b11e164580c0c768d
Gaurav Pratap Singh <gs492979@gmail.com>
2023-07-20 02:12:39 +0530

    in Logo img alt attribute added.

--- src/components/Header-section/Navbar.jsx
+++ src/components/Header-section/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (
         <>
             <nav className="navbar flex justify-between items-center p-4">
-                <img src="./src/assets/logo.png" alt="" />
+                <img src="./src/assets/logo.png" alt="Rentalog-logo" />
                 <ul className='flex'>
                     <li className='nav-links mr-9 p-3 '>HOME</li>
                     <li className='nav-links mr-9 p-3'>ABOUT</li>
