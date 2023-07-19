189a5a14d7ed8fa781b8454c6a825e4ddc1d988f
Gaurav Pratap Singh <gs492979@gmail.com>
2023-07-19 13:25:43 +0530

    file structure updated.

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './src/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
