c530f943ce68a5427fff3a6c5f9510cee051802e
Divya Kumari <kumaridivya2796@gmail.com>
2023-07-21 12:36:06 +0530

    added footer

--- src/components/Header-section/Header.jsx
+++ src/components/Header-section/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
     return (
         <>
             <div className="header-section">
-                <Navbar />
+                {/* <Navbar /> */}
                 <div className="header-text">
                     <h1>Welcome to <span>Rentalog</span></h1>
-                    <p>Rentalog is the ultimate solution for landlords looking to simplify their rent management process. Keep track of your renters' personal details, their payment history, and much more with ease.</p>
+                    <p>Rentalog is the ultimate solution for landlords looking to simplify their rent management process. Keep track of your renters personal details, their payment history, and much more with ease.</p>
                     <button className='btn'>Get Started</button>
                 </div>
             </div>
