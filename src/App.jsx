b5a471acd9d6102fe48dabdac4e37750680f0379
kashif <kashif_saleem_23@hotmail.com>
2023-07-22 01:10:01 +0500

    Feat: Imported and rendered contact section changes pulled from remote repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import './App.css'
-import Footer from './components/Footer-section/Footer'
-import Header from './components/Header-section/Header'
-import Connect from './components/connectUs-section/Connect'
-import AboutUs from './components/AboutUs-section/AboutUs';
-import Services from './components/Services-section/Services-section';
-const App = () => {
+import "./App.css";
+import Footer from "./components/Footer-section/Footer";
+import Header from "./components/Header-section/Header";
+import Connect from "./components/connectUs-section/Connect";
+import AboutUs from "./components/AboutUs-section/AboutUs";
+import Services from "./components/Services-section/Services-section";
+import "./App.css";
+import Contact from "./components/Header-section/Contact-section/Contact";
 
+const App = () => {
   return (
     <>
       <Header />
       <Services />
       <AboutUs />
       <Connect />
+      <Contact />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
