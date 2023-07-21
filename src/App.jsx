87a6f74bd42e0a0ceb70cb388deb19059e5fc1b5
Roy404Dev <rojusbalciunas@gmail.com>
2023-07-21 12:51:25 +0300

    about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header-section/Header'
-
+import AboutUs from './components/AboutUs-section/AboutUs';
 const App = () => {
 
   return (
     <>
       <Header />
+      <AboutUs />
       <Footer />
     </>
   )
