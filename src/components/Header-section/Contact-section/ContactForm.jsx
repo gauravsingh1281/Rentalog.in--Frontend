ad3cf8b60a4e4cb92140c22eeefb7893d75a911e
kashif <kashif_saleem_23@hotmail.com>
2023-07-22 01:10:49 +0500

    Feat: Styled the contact form

---
+++ src/components/Header-section/Contact-section/ContactForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./ContactForm.module.css";
+
+const ContactForm = () => {
+  return (
+    <>
+      <div className={style.ContactForm}>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default ContactForm;
