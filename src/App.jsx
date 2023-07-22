c00afd825300abc8b0f99e13a72689e1eb63fa0b
Divya Kumari <kumaridivya2796@gmail.com>
2023-07-22 15:36:49 +0530

    added connect with us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header-section/Header'
+import Connect from './components/connectUs-section/Connect'
 
 const App = () => {
 
   return (
     <>
       <Header />
+      <Connect />
       <Footer />
     </>
   )
