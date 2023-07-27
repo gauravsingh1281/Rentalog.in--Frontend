e3722c3da50fd6ec79041973c6351c92558beb39
kashif <kashif_saleem_23@hotmail.com>
2023-07-27 19:23:30 +0500

    Feat: Form section added

--- src/components/Contact-section/ContactForm.jsx
+++ src/components/Contact-section/ContactForm.jsx
@@ -1,27 +1,53 @@
-import React from "react";
-import style from "./ContactForm.module.css";
 import Github from "../../assets/Social-Icons/Github.png";
-import Insta from "../../assets/Social-Icons/Insta.png";
+import Insta from "../../assets/Social-Icons/instagram.png";
 import Twitter from "../../assets/Social-Icons/Twitter.png";
+
 const ContactForm = () => {
   return (
     <>
-      <section className={style.ContactForm}>
-        <article>
-          <h2>Connect with Us</h2>
+      <section className="flex flex-col md:flex-row gap-y-10 my-20">
+        {/* left hand side of the form */}
+        <article className="flex-1 ml-24">
+          <h2 className="text-6xl font-bold tracking-wider mb-10 font-monsterrat text-textBlack">
+            Connect with <span className="text-customRed">Us</span>
+          </h2>
           <div
-            className="flex gap-x-5 j h-4 container justify-center align-baseline"
-            style={{ height: "min-content" }}
+            className="flex gap-x-12"
+            style={{ height: "min-content", alignItems: "baseline" }}
           >
-            <img src={Github} alt="" style={{ height: "5vh" }} />
-            <img src={Insta} alt="" style={{ height: "8vh" }} />
-            <img src={Twitter} alt="" style={{ height: "5vh" }} />
+            <img src={Github} alt="" style={{ height: "6vh" }} />
+            <img src={Insta} alt="" style={{ height: "6vh" }} />
+            <img src={Twitter} alt="" style={{ height: "6vh" }} />
           </div>
         </article>
+        {/* Right hand side of the form */}
+        <article className="flex flex-1 justify-center mr-24">
+          <form className="gap-x-5 w-full">
+            <div className="flex flex-col md:flex-row mb-6 justify-between">
+              <input
+                className="bg-green rounded-lg text-textBlack h-10 w-5/12 "
+                placeholder="First name"
+              />
+              <input
+                className="bg-green rounded-lg h-10 w-5/12"
+                placeholder="Last name"
+              />
+            </div>
+            <div className="flex flex-col md:flex-row mb-6 justify-between">
+              <input
+                className="bg-green rounded-lg h-10 w-5/12"
+                placeholder="First name"
+              />
+              <input
+                className="bg-green rounded-lg w-5/12"
+                placeholder="Last name"
+              />
+            </div>
 
-        <article>
-          <form>
-            <div></div>
+            <textarea
+              className="bg-green rounded-lg w-full h-40"
+              placeholder="Your message here"
+            />
           </form>
         </article>
       </section>
