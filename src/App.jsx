0322c8b14b5d12abc75f0ccc6bfe24fef65b6bf4
kashif <kashif_saleem_23@hotmail.com>
2023-07-28 14:44:19 +0500

    Fix: Issue with file path and duplicated contact section removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import Contact from "./components/Contact-section/Contact";
 import Header from "./components/Header-section/Header";
+import Footer from "./components/Footer-section/Footer";
+import AboutUs from "./components/AboutUs-section/AboutUs";
+import Services from "./components/Services-section/Services-section";
 
 const App = () => {
   return (
@@ -8,7 +11,6 @@ const App = () => {
       <Header />
       <Services />
       <AboutUs />
-      <Connect />
       <Contact />
       <Footer />
     </>
