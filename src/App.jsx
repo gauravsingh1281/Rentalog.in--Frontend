5f2b52c4bf42a7ecf552ed623fffc2b7f586d879
kashif <kashif_saleem_23@hotmail.com>
2023-08-21 16:44:09 +0500

    Feat: Setup router and add packages for admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import './App.css'
-import Footer from './components/Footer-section/Footer'
-import Header from './components/Header-section/Header'
-import Connect from './components/connectUs-section/Connect'
-import AboutUs from './components/AboutUs-section/AboutUs';
-import Services from './components/Services-section/Services-section';
-const App = () => {
+import "./App.css";
+import Footer from "./components/Footer-section/Footer";
+import Header from "./components/Header-section/Header";
+import Connect from "./components/connectUs-section/Connect";
+import AboutUs from "./components/AboutUs-section/AboutUs";
+import Services from "./components/Services-section/Services-section";
+import Dashboard from "./components/Admin-Dashboard/Dashboard";
 
+const App = () => {
   return (
     <>
       <Header />
       <Services />
+      
       <AboutUs />
       <Connect />
       <Footer />
+      <section id="dashboard">
+        <Dashboard />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Header-section/Navbar.jsx
+++ src/components/Header-section/Navbar.jsx
@@ -1,23 +1,52 @@
-import logo from "../../assets/Images/logo.png"
-import { FiUser } from 'react-icons/fi';
+import logo from "../../assets/Images/logo.png";
+import { FiUser } from "react-icons/fi";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-    return (
-        <>
-            <nav className="navbar flex justify-between items-center py-4 px-12">
-                <img src={logo} alt="Rentalog-logo" />
-                <ul className='flex'>
-                    <li className='nav-links mr-5 p-3 '>HOME</li>
-                    <li className='nav-links mr-5 p-3'>ABOUT</li>
-                    <li className='nav-links mr-5 p-3'>CONTACT</li>
-                    <li className='nav-links  p-3'>RENTALS</li>
-                </ul>
-                <ul className='user-cta flex'>
-                    <li className='mr-5 p-3'>Login</li>
-                    <li className=' p-3'><FiUser className="mr-1" />Register</li>
-                </ul>
-            </nav>
-        </>
-    )
-}
+  return (
+    <>
+      <nav className="navbar flex justify-between items-center py-4 px-12">
+        <Link to="home" smooth={true} duration={500}>
+          <img src={logo} alt="Rentalog-logo" />
+        </Link>
+
+        <ul className="flex">
+          <li className="nav-links mr-5 p-3 ">
+            <Link to="home" smooth={true} duration={500}>
+              HOME
+            </Link>
+          </li>
+          <li className="nav-links mr-5 p-3">
+            <Link to="about" smooth={true} duration={500}>
+              ABOUT
+            </Link>
+          </li>
+          <li className="nav-links mr-5 p-3">
+            <Link to="contact" smooth={true} duration={500}>
+              CONTACT
+            </Link>
+          </li>
+          <li className="nav-links  p-3">
+            <Link to="rentals" smooth={true} duration={500}>
+              RENTALS
+            </Link>
+          </li>
+          <li className="nav-links  p-3">
+            <Link to="dashboard" smooth={true} duration={500}>
+              {" "}
+              DASHBOARD
+            </Link>
+          </li>
+        </ul>
+        <ul className="user-cta flex">
+          <li className="mr-5 p-3">Login</li>
+          <li className=" p-3">
+            <FiUser className="mr-1" />
+            Register
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 export default Navbar;
