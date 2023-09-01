f2d7d0bf0f264994ef0b6794a95f6b6919917133
kashif <kashif_saleem_23@hotmail.com>
2023-09-01 22:15:04 +0500

    Fix: fixed navbar

--- src/components/Header-section/Navbar.jsx
+++ src/components/Header-section/Navbar.jsx
@@ -7,12 +7,12 @@ import MenuIcon from "../../assets/Icons/menu-icon.png";
 const Navbar = ({ click }) => {
   return (
     <>
-      <nav className="navbar flex justify-between items-center py-4 px-12">
+      <nav className="navbar flex justify-between items-center y-4 px-4 md:py-4 md:px-12">
         <Link to="/" smooth={true} duration={500}>
           <img src={logo} alt="Rentalog-logo" />
         </Link>
 
-        <ul className="flex">
+        <ul className="hidden md:flex">
           <li className="nav-links mr-5 p-3 ">
             <Link to="/" smooth={true} duration={500}>
               HOME
@@ -40,9 +40,11 @@ const Navbar = ({ click }) => {
             DASHBOARD
           </li>
         </ul>
-        <ul className="user-cta flex">
-          <li className="mr-5 p-3">Login</li>
-          <li className=" p-3">
+        <ul className="user-cta hidden md:flex">
+          <Link to="/login">
+            <li className="mr-5 p-2 md:p-3">Login</li>
+          </Link>
+          <li className=" p-2 md:p-3">
             <FiUser className="mr-1" />
             Register
           </li>
