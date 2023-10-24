d06e629454e60c43fcc2eebbcaf0dbf84b234b10
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-10-24 10:42:33 +0530

    Restructereing the contact section

--- src/components/Contact-section/ContactForm.jsx
+++ src/components/Contact-section/ContactForm.jsx
@@ -5,28 +5,7 @@ import Twitter from "../../assets/Social-Icons/Twitter.png";
 const ContactForm = () => {
   return (
     <>
-      <section className="flex flex-col md:flex-row  gap-y-10 my-20">
-        {/* left hand side of the form */}
-        <article className="flex-1 text-center md:text-left md:ml-24">
-          <h2 className="text-3xl lg:text-5xl font-bold tracking-wider mb-10 font-monsterrat text-textBlack">
-            Connect with <span className="text-customRed">Us</span>
-          </h2>
-          <div
-            className="flex gap-x-12 justify-center md:justify-start"
-            style={{ height: "min-content", alignItems: "baseline" }}
-          >
-            <a href="https://twitter.com/gauravsingh1281">
-              <img src={Twitter} alt="Twitter-logo" className="h-14" />
-            </a>
-            <a href="https://github.com/gauravsingh1281">
-              <img src={Github} alt="Github-logo" className="h-14" />
-            </a>
-            <a href="https://www.instagram.com/gauravsingh1281">
-              <img src={Insta} alt="Insta-logo" className="h-14" />
-            </a>
-          </div>
-        </article>
-        {/* Right hand side of the form */}
+      <section className="lg:w-[45%] w-full">
         <article className="flex flex-1 justify-center p-5 md:p-0 md:mr-24">
           <form className="gap-x-5 w-full">
             <div className="flex flex-col md:flex-row mb-6 justify-between">
