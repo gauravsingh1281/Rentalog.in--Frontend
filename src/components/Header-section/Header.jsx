d883595f3affc8d20d44c9f2eee748cf3bb2f03b
David <davidalrojas@gmail.com>
2023-10-28 16:04:52 -0500

    fix mobile layout of the main page

--- src/components/Header-section/Header.jsx
+++ src/components/Header-section/Header.jsx
@@ -1,24 +1,27 @@
-import '../Header-section/Header-section.css'
+import "../Header-section/Header-section.css";
 import { Link } from "react-router-dom";
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 const Header = () => {
-
-
-    
-    return (
-        <>
-            <div className="header-section mb-[80px]">
-                <Navbar />
-                <div id='home'></div>
-                <div className="header-text">
-                    <h1>Welcome to <span>Rentalog</span></h1>
-                    <p>Rentalog is the ultimate solution for landlords looking to simplify their rent management process. Keep track of your renters personal details, their payment history, and much more with ease.</p>
-                    <Link to="/register">
-                        <button className='btn'>Get Started</button>
-                    </Link>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="header-section mb-[80px]">
+        <Navbar />
+        <div id="home"></div>
+        <div className="header-text top-[15%] sm:top-[23%]">
+          <h1>
+            Welcome to <span>Rentalog</span>
+          </h1>
+          <p>
+            Rentalog is the ultimate solution for landlords looking to simplify
+            their rent management process. Keep track of your renters personal
+            details, their payment history, and much more with ease.
+          </p>
+          <Link to="/register">
+            <button className="btn">Get Started</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Header;
