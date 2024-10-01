b2ca0a8a650f184059b0bcb5d3949b4b8c7992a3
Om Jadhav <om427880@gmail.com>
2024-10-01 22:41:17 +0530

    Responsiveness of social icons and redesign the contact us

--- src/components/Contact-section/ContactText.jsx
+++ src/components/Contact-section/ContactText.jsx
@@ -1,40 +1,31 @@
 import React from 'react'
-import Github from "../../assets/Social-Icons/Github.png";
-import Insta from "../../assets/Social-Icons/instagram.png";
-import Twitter from "../../assets/Social-Icons/Twitter.png";
 
 export const ContactText = () => {
     return (
-        <section className='flex flex-col gap-[20px] justify-center lg:w-[45%] w-full'>
-            <div className="">
-                Hey Rentalog Users, <br />
-                We are always looking forward to hearing from you.
-                Please feel free to reach out to us for any queries, suggestions, or
-                feedback. <br />
-                We would love to hear from you. <br />
-                <br />
-                Contact us: <br />
-                Email: <a href="mailto:support@rentalog.in" className='font-bold text-primaryGreen underline'> support@rentalog.in </a> <br />
-                Phone: <a href="tel:+916201577047" className='font-bold text-primaryGreen underline'>  +91 62015 77047 </a> <br />
-                Address: Shaktinagar, Sonebhadra, UP, 231222 <br />
-                <br />
-                We welcome your input to improve your Rentalog experience. <br />
-                Thank you for choosing Rentalog! <br />
-                Best regards, <br />
-                The Rentalog Team
-            </div>
-            <div
-                className="flex gap-x-12 lg:justify-center justify-start"
-            >
-                <a href="https://twitter.com/gauravsingh1281">
-                    <img src={Twitter} alt="Twitter-logo" className="h-8" />
-                </a>
-                <a href="https://github.com/gauravsingh1281">
-                    <img src={Github} alt="Github-logo" className="h-8" />
-                </a>
-                <a href="https://www.instagram.com/gauravsingh1281">
-                    <img src={Insta} alt="Insta-logo" className="h-8" />
-                </a>
+        <section className="bg-gray-100  rounded-lg shadow-lg flex flex-col gap-6 justify-center lg:w-[45%] w-full">
+            <div className="text-gray-800">
+                <h2 className="text-xl font-bold mb-4">
+                    Hey Rentalog Users,
+                </h2>
+                <p className="leading-relaxed">
+                    We are always looking forward to hearing from you.
+                    Please feel free to reach out to us for any queries, suggestions, or
+                    feedback. <br />
+                    We would love to hear from you.
+                </p>
+
+                <div className="mt-4">
+                    <p className="font-semibold">Contact us:</p>
+                    <p>Email: <a href="mailto:support@rentalog.in" className="font-bold text-primaryGreen underline"> support@rentalog.in </a></p>
+                    <p>Phone: <a href="tel:+916201577047" className="font-bold text-primaryGreen underline"> +91 62015 77047 </a></p>
+                    <p>Address: Shaktinagar, Sonebhadra, UP, 231222</p>
+                </div>
+
+                <p className="mt-4 leading-relaxed">
+                    We welcome your input to improve your Rentalog experience. <br />
+                    Thank you for choosing Rentalog!
+                </p>
+                <p className="mt-2">Best regards,<br />The Rentalog Team</p>
             </div>
         </section>
     )
