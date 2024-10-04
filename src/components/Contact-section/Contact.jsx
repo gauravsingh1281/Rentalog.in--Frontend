96fed9125eb648eb23985ff2f8e4b6a59bb8f6e4
DivyaJyothi9 <liladivyajyothiakula@gmail.com>
2024-10-04 17:54:43 +0530

    updated the contact page layout

--- src/components/Contact-section/Contact.jsx
+++ src/components/Contact-section/Contact.jsx
@@ -8,15 +8,17 @@ import Twitter from "../../assets/Social-Icons/Twitter.png";
 
 const Contact = () => {
   return (
-    <section className="flex flex-col w-full  px-[5%] gap-10 justify-center">
+    <section className="flex flex-col w-full px-[5%] gap-10 justify-center">
       <header id="ContactUs" className="flex justify-center align-middle mt-20">
         <h1 className="text-5xl font-bold tracking-wider text-text-black font-monsterrat">
           Contact Us
         </h1>
       </header>
-      <main className=" flex flex-wrap lg:gap-[5%] gap-[20px] justify-center items-center">
-        <ContactText />
-        <ContactForm />
+      <main className="flex flex-wrap lg:gap-[5%] gap-[20px] justify-center items-center">
+        <div className="contact-box flex flex-col md:flex-row lg:gap-[5%] gap-[20px] justify-center items-center">
+          <ContactText />
+          <ContactForm />
+        </div>
       </main>
       <div className="flex items-center justify-center p-0">
         <article className="flex flex-col items-center md:items-start text-center md:text-left">
