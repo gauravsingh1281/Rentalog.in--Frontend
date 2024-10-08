c9245c4352bf82b824553726b4de479c3c1ba214
Asghar Hussain <asghus117@gmail.com>
2024-10-08 22:18:27 +0530

    added carousel

--- src/components/Header-section/Header.jsx
+++ src/components/Header-section/Header.jsx
@@ -1,19 +1,39 @@
 import "../Header-section/Header-section.css";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import carImg1 from "../../assets/Images/red-house.png"
+import carImg2 from "../../assets/Images/carousel2.png"
+import carImg3 from "../../assets/Images/carousel3.png"
 const Header = () => {
   return (
     <>
       <div className="header-section mb-[80px]">
         <Navbar />
+
         <div id="home"></div>
-        <div className="header-text top-[15%] sm:top-[23%]">
+        <Carousel autoPlay = {true} infiniteLoop = {true} showStatus = {false} showThumbs = {false} stopOnHover ={false} swipeable = {false} animationHandler="fade">
+          <div>
+            <img src={carImg1} />
+            
+          </div>
+          <div>
+            <img src={carImg2} />
+            
+          </div>
+          <div>
+            <img src={carImg3} />
+            
+          </div>
+        </Carousel>
+        <div className="header-text top-[15%] sm:top-[23%] absolute">
           <h1>
             Welcome to <span>Rentalog</span>
           </h1>
           <p>
-            Rentalog is the ultimate solution for landlords looking to simplify
-            their rent management process. Keep track of your renters personal
+            Rentalog is the ultimate solution for landlords looking to simplify <br />
+            their rent management process. Keep track of your <br /> renters personal
             details, their payment history, and much more with ease.
           </p>
           <Link to="/register">
