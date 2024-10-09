6427128749962be50c02990b96b311907516df88
Asghar Hussain <asghus117@gmail.com>
2024-10-10 02:34:19 +0530

    added animation on scroll

--- src/components/Services-section/Services-section.jsx
+++ src/components/Services-section/Services-section.jsx
@@ -18,7 +18,7 @@ const Services = () => {
         Our Services Include
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 my-10 md:mx-24 lg:mx-36 sm:mx-24 mx-8">
-        {ourServices.map(({ id, bgColor, title, description, icon, alt }) => {
+        {ourServices.map(({ id, bgColor, title, description, icon, alt , aos }) => {
           return (
             <div
               key={id}
@@ -31,6 +31,7 @@ const Services = () => {
                 icon={icon}
                 alt={alt}
                 description={description}
+                aos = {aos}
               />
             </div>
           );
