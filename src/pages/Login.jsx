50887f5c65def02b8c027b383a127f42ee81b359
AtharvGaur05 <laatharv@gmail.com>
2024-10-11 23:32:36 +0530

    added-crossicon-login/signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,6 +10,7 @@ export default function Login() {
   });
 
   const [errors, setErrors] = useState({});
+
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
@@ -19,17 +20,17 @@ export default function Login() {
     const validationErrors = {};
 
     if (!form.email.trim()) {
-      validationErrors.email = "email is required";
+      validationErrors.email = "Email is required";
     }
 
     if (!form.password.trim()) {
-      validationErrors.password = "password is required";
+      validationErrors.password = "Password is required";
     }
 
     setErrors(validationErrors);
 
     if (Object.keys(validationErrors).length === 0) {
-      // alert("Login Succesful");
+      // alert("Login Successful");
       navigate("/home");
       setForm({
         email: "",
@@ -38,31 +39,38 @@ export default function Login() {
     }
   };
 
+  const closeLogin = () => {
+    navigate("/home"); // Redirect to the home page on closing the login form
+  };
+
   return (
-    <article className="flex flex-col justify-center h-[100vh]  bg-primaryGreen/10 overflow-hidden">
+    <article className="flex flex-col justify-center h-[100vh] bg-primaryGreen/10 overflow-hidden">
       <Navbar />
       <br className=" hidden xl:block"></br>
-      <section className="flex flex-row justify-baseline items-center ">
-        <div className="hidden w-full lg:flex lg:mt-12 flex-col lg:justify-start xl:justify-center  xl:h-full  ">
+      <section className="flex flex-row justify-baseline items-center">
+        <div className="hidden w-full lg:flex lg:mt-12 flex-col lg:justify-start xl:justify-center xl:h-full">
           <div className="mb-10 mx-10 mr-auto">
-            <h2 className=" text-3xl font-bold text-gray-dark/90 ">
-              <span className="text-customRed italic"> Best way</span> to manage
-              you rent
+            <h2 className="text-3xl font-bold text-gray-dark/90">
+              <span className="text-customRed italic">Best way</span> to manage your rent
             </h2>
 
-            <p className="mt-2 text-gray-dark/70 ">
+            <p className="mt-2 text-gray-dark/70">
               Enter your credentials to access your account
             </p>
           </div>
-          <img
-            src="undraw_for_sale_re_egkk.svg"
-            alt=""
-            className="m-10 w-auto"
-          />
+          <img src="undraw_for_sale_re_egkk.svg" alt="" className="m-10 w-auto" />
         </div>
 
-        <div className="flex mx-auto max-w-7xl w-full lg:w-[75vw] h-screen xl:h-fit justify-between  lg:rounded-bl-3xl lg:rounded-tl-3xl  bg-primaryGreen/10 ">
-          <div className="w-full h-[100vh] flex flex-col justify-start mt-20  lg:mt-0 lg:justify-center items-center">
+        <div className="flex mx-auto max-w-7xl w-full lg:w-[75vw] h-screen xl:h-fit justify-between lg:rounded-bl-3xl lg:rounded-tl-3xl bg-primaryGreen/10">
+          <div className="w-full h-[100vh] flex flex-col justify-start mt-20 lg:mt-0 lg:justify-center items-center relative">
+            {/* Close Icon */}
+            <span
+              className="absolute top-5 right-5 text-2xl cursor-pointer font-bold text-red-600"
+              onClick={closeLogin}
+            >
+              &times;
+            </span>
+
             <TitleCard />
 
             <form
@@ -78,9 +86,7 @@ export default function Login() {
                 onChange={handleChange}
                 className="input-bar"
               />
-              <span className="pl-4 text-[#ff0000] text-sm">
-                {errors.email}
-              </span>
+              <span className="pl-4 text-[#ff0000] text-sm">{errors.email}</span>
               <Input
                 title="Password"
                 name="password"
@@ -90,9 +96,7 @@ export default function Login() {
                 onChange={handleChange}
                 className="input-bar"
               />
-              <span className="pl-4 text-[#ff0000] text-sm">
-                {errors.password}
-              </span>
+              <span className="pl-4 text-[#ff0000] text-sm">{errors.password}</span>
               <Link
                 to="#"
                 className="float-right relative bottom-3 text-green hover:underline"
@@ -112,19 +116,18 @@ export default function Login() {
               <div className="flex flex-col justify-center">
                 <div className="flex flex-row justify-center items-center">
                   <hr className="w-1/4  text-gray/60"></hr>
-                  <p className=" text-gray text-sm text-center">
+                  <p className="text-gray text-sm text-center">
                     &nbsp;Or Login Using&nbsp;
                   </p>
                   <hr className="w-1/4 text-gray/60"></hr>
                 </div>
 
-                <div className=" flex flex-row md:flex-row items-center space-x-2 justify-center mt-5 lg:mt-2 ">
+                <div className="flex flex-row md:flex-row items-center space-x-2 justify-center mt-5 lg:mt-2">
                   <button
                     type="button"
                     className="border bg-textWhite focus:shadow-md border-[#c7c5c5] w-[30%] py-1.5 rounded-xl text-black mt-2 flex items-center justify-center px-2 h-10"
                   >
                     <img
-                      // src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                       src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
                       alt="Google logo"
                       className="h-5"
@@ -133,7 +136,7 @@ export default function Login() {
 
                   <button
                     type="button"
-                    className="border bg-textWhite focus:shadow-md border-[#c7c5c5] w-[30%] py-1.5 rounded-xl text-black mt-2 flex items-center justify-center  px-2 h-10"
+                    className="border bg-textWhite focus:shadow-md border-[#c7c5c5] w-[30%] py-1.5 rounded-xl text-black mt-2 flex items-center justify-center px-2 h-10"
                   >
                     <img
                       src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Facebook_icon.svg"
@@ -148,7 +151,7 @@ export default function Login() {
                 Don&rsquo;t have any account?{" "}
                 <Link
                   to="/register"
-                  className=" text-primaryGreen font-semibold hover:text-primaryGreen/60"
+                  className="text-primaryGreen font-semibold hover:text-primaryGreen/60"
                 >
                   Sign Up
                 </Link>
@@ -174,8 +177,8 @@ const Navbar = () => {
 const TitleCard = () => {
   return (
     <div className="flex flex-col">
-      <h2 className="mt-16 text-left text-3xl font-extrabold leading-9 tracking-tight  text-gray-dark/90 ">
-        <span className=" text-customRed text-4xl">Welcome</span>
+      <h2 className="mt-16 text-left text-3xl font-extrabold leading-9 tracking-tight text-gray-dark/90">
+        <span className="text-customRed text-4xl">Welcome</span>
         <span> </span>back!!
       </h2>
 
@@ -187,7 +190,6 @@ const TitleCard = () => {
 };
 
 const Input = (props) => {
-  // eslint-disable-next-line react/prop-types
   const { ...rest } = props;
   return (
     <div className="flex flex-col">
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/Images/logo.png";
 
 export default function Registration() {
+  const navigate = useNavigate(); // Use the navigate hook for redirection
   const [form, setForm] = useState({
     name: "",
     email: "",
@@ -12,7 +13,7 @@ export default function Registration() {
 
   const [errors, setErrors] = useState({});
 
-  const handlechange = (e) => {
+  const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
@@ -20,25 +21,25 @@ export default function Registration() {
     e.preventDefault();
     const validationErrors = {};
     if (!form.name.trim()) {
-      validationErrors.username = "username is required";
+      validationErrors.username = "Username is required";
     }
 
     if (!form.email.trim()) {
-      validationErrors.email = "email is required";
+      validationErrors.email = "Email is required";
     }
     if (!/\S+@\S+\.\S+/.test(form.email)) {
-      validationErrors.email = "email is not valid";
+      validationErrors.email = "Email is not valid";
     }
     if (!form.password.trim()) {
-      validationErrors.password = "password is required";
+      validationErrors.password = "Password is required";
     } else if (form.password.length < 6) {
-      validationErrors.password = "password should be at least 6 characters";
+      validationErrors.password = "Password should be at least 6 characters";
     }
 
     if (!form.confirmPassword.trim()) {
-      validationErrors.confirmPassword = "password confirmation is required";
+      validationErrors.confirmPassword = "Password confirmation is required";
     } else if (form.confirmPassword !== form.password) {
-      validationErrors.confirmPassword = "password not matched";
+      validationErrors.confirmPassword = "Passwords do not match";
     }
 
     setErrors(validationErrors);
@@ -54,26 +55,38 @@ export default function Registration() {
     }
   };
 
+  const closeRegistration = () => {
+    navigate("/home"); // Redirect to home page on clicking the close icon
+  };
+
   return (
     <article className="flex flex-col justify-center h-screen bg-primaryGreen/10 overflow-hidden">
       <Navbar />
       <section className="flex flex-row justify-center items-center">
-        <div className="hidden w-full lg:flex lg:mt-12 flex-col justify-center xl:justify-center  xl:h-full ">
+        <div className="hidden w-full lg:flex lg:mt-12 flex-col justify-center xl:justify-center xl:h-full">
           <div className="mb-10 mx-10 mr-auto">
-            <h2 className=" text-3xl font-bold text-gray-dark/90 ">
+            <h2 className="text-3xl font-bold text-gray-dark/90">
               <span className="text-customRed italic"> Best way</span> to manage
-              you rent
+              your rent
             </h2>
 
-            <p className="mt-2  text-gray-dark/70 ">
+            <p className="mt-2 text-gray-dark/70">
               Create a new account to access all the features of our website
             </p>
           </div>
           <img src="undraw_for_sale_re_egkk.svg" alt="" className="m-10" />
         </div>
 
-        <div className="flex mx-auto max-w-7xl w-full lg:w-[75vw] h-[100vh]   xl:h-fit justify-between  lg:rounded-bl-3xl lg:rounded-tl-3xl  bg-primaryGreen/10 ">
-          <div className="w-full h-[100vh] flex flex-col justify-start mt-20 lg:mt-0 lg:justify-center items-center">
+        <div className="flex mx-auto max-w-7xl w-full lg:w-[75vw] h-[100vh] xl:h-fit justify-between lg:rounded-bl-3xl lg:rounded-tl-3xl bg-primaryGreen/10">
+          <div className="w-full h-[100vh] flex flex-col justify-start mt-20 lg:mt-0 lg:justify-center items-center relative">
+            {/* Close Icon */}
+            <span
+              className="absolute top-5 right-5 text-2xl cursor-pointer font-bold text-red-600"
+              onClick={closeRegistration}
+            >
+              &times;
+            </span>
+
             <TitleCard />
             <form
               onSubmit={handleSubmit}
@@ -85,7 +98,7 @@ export default function Registration() {
                 value={form.name}
                 type="text"
                 placeholder="Name"
-                onChange={handlechange}
+                onChange={handleChange}
                 className="input-bar"
               />
               {errors.username && (
@@ -100,13 +113,11 @@ export default function Registration() {
                 value={form.email}
                 type="email"
                 placeholder="E-mail Address"
-                onChange={handlechange}
+                onChange={handleChange}
                 className="input-bar"
               />
               {errors.email && (
-                <span className="pl-4 text-[#ff0000] text-sm">
-                  {errors.email}
-                </span>
+                <span className="pl-4 text-[#ff0000] text-sm">{errors.email}</span>
               )}
               <Input
                 title="Password"
@@ -114,7 +125,7 @@ export default function Registration() {
                 value={form.password}
                 type="password"
                 placeholder="Password"
-                onChange={handlechange}
+                onChange={handleChange}
                 className="input-bar"
               />
               {errors.password && (
@@ -128,7 +139,7 @@ export default function Registration() {
                 type="password"
                 placeholder="Confirm Password"
                 value={form.confirmPassword}
-                onChange={handlechange}
+                onChange={handleChange}
                 className="input-bar"
               />
               {errors.confirmPassword && (
@@ -147,8 +158,8 @@ export default function Registration() {
 
               <div className="flex flex-col justify-center">
                 <div className="flex flex-row justify-center items-center">
-                  <hr className="w-1/4  text-gray/60"></hr>
-                  <p className=" text-gray text-sm text-center">
+                  <hr className="w-1/4 text-gray/60"></hr>
+                  <p className="text-gray text-sm text-center">
                     &nbsp;Or Connect with&nbsp;
                   </p>
                   <hr className="w-1/4 text-gray/60"></hr>
@@ -157,10 +168,9 @@ export default function Registration() {
                 <div className="flex flex-row md:flex-row items-center space-x-2 justify-center">
                   <button
                     type="button"
-                    className="border bg-textWhite focus:shadow-md  lg:hover:shadow-md border-[#c7c5c5] w-[30%] py-1.5 rounded-xl text-black mt-1 flex items-center justify-center px-2 h-10"
+                    className="border bg-textWhite focus:shadow-md lg:hover:shadow-md border-[#c7c5c5] w-[30%] py-1.5 rounded-xl text-black mt-1 flex items-center justify-center px-2 h-10"
                   >
                     <img
-                      // src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                       src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
                       alt="Google logo"
                       className="h-5"
@@ -169,7 +179,7 @@ export default function Registration() {
 
                   <button
                     type="button"
-                    className="border bg-textWhite focus:shadow-md  lg:hover:shadow-md border-[#c7c5c5] w-[30%] py-1.5 rounded-xl text-black mt-1 flex items-center justify-center px-2 h-10"
+                    className="border bg-textWhite focus:shadow-md lg:hover:shadow-md border-[#c7c5c5] w-[30%] py-1.5 rounded-xl text-black mt-1 flex items-center justify-center px-2 h-10"
                   >
                     <img
                       src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Facebook_icon.svg"
@@ -184,7 +194,7 @@ export default function Registration() {
                 Already have an account?{" "}
                 <Link
                   to="/login"
-                  className=" text-primaryGreen font-semibold hover:text-primaryGreen/60"
+                  className="text-primaryGreen font-semibold hover:text-primaryGreen/60"
                 >
                   Login
                 </Link>
@@ -199,21 +209,22 @@ export default function Registration() {
 
 const Navbar = () => {
   return (
-    <div className="absolute  top-4 left-4">
+    <div className="absolute top-4 left-4">
       <Link to="/">
         <img src={logo} alt="Rentalog-logo" className="h-12 w-auto" />
       </Link>
     </div>
   );
 };
+
 const TitleCard = () => {
   return (
-    <div className="">
-      <h2 className=" lg:mt-10 text-left text-3xl font-extrabold leading-9 tracking-tight  text-gray-dark/90 ">
-        Welcome to <span className=" text-customRed text-4xl">Rentlog</span>
+    <div>
+      <h2 className="lg:mt-10 text-left text-3xl font-extrabold leading-9 tracking-tight text-gray-dark/90">
+        Welcome to <span className="text-customRed text-4xl">Rentlog</span>
       </h2>
 
-      <p className="text-left  text-gray text-sm mt-1">
+      <p className="text-left text-gray text-sm mt-1">
         Create a new account to access all the features <br></br>of our website
       </p>
     </div>
@@ -221,7 +232,6 @@ const TitleCard = () => {
 };
 
 const Input = (props) => {
-  // eslint-disable-next-line react/prop-types
   const { ...rest } = props;
   return (
     <div className="flex flex-col">
